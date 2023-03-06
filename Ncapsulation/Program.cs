ec19f257a799b74748ed2782158ea159ffd7b819
F4khr1gjr <fakhrimg.code.edu.az>
2023-03-07 01:59:33 +0400

    Ncapsulation zornan yaradildi

---
+++ Ncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("User Name daxil edin");
+string UserName = Console.ReadLine();
+Console.WriteLine("Password daxil edin");
+string Password = Console.ReadLine();
+
+
+User user = new User(UserName, Password);
+
+
+
+if (user.Password != null && user.UserName != null)
+{
+    Console.WriteLine("Dogrudur");
+}
+else
+{
+    Console.WriteLine("Yanlisdir");
+}
---
+++ Ncapsulation/User.cs
@@ -0,0 +1,44 @@
+﻿internal class User
+{
+    private string _UserName;
+    private string _Password;
+    public string UserName
+    {
+        get { return _UserName; }
+        set { _UserName = value; }
+    }
+    public string Password
+    {
+        get { return _Password; }
+        set
+        {
+            if (CheckPassword(value) == true)
+            {
+                _Password = value;
+            }
+        }
+
+    }
+
+    public User(string userName, string password)
+    {
+        this.UserName = userName;
+        this.Password = password;
+    }
+
+
+    public bool CheckPassword(string password)
+    {
+        bool status = false;
+        if (password.Length > 7 && password.Any(char.IsUpper))
+        {
+            status = true;
+        }
+        else
+        {
+            Console.WriteLine("Zehmet olmasa 1 herfi boyuk olan minimum 8 simvollu password daxil edin");
+        }
+        return status;
+    }
+
+}
